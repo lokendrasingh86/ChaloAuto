d077cf08eb8fc74a8025ae3913b1457136dd6582
lokendrasingh86 <lokendrasinghsps08@gmail.com>
2026-04-14 23:34:44 +0530

    Updation done

--- Backend/src/services/ride.service.ts
+++ Backend/src/services/ride.service.ts
@@ -1,12 +1,27 @@
 
 import { prisma } from "../lib/prisma.ts";
 
-function distance(a: { latitude: number; longitude: number }, b: { latitude: number; longitude: number }) {
-    return Math.sqrt(
-        Math.pow(a.latitude - b.latitude, 2) + 
-        Math.pow(a.longitude - b.longitude, 2)
-    );
+// function distance(a: { latitude: number; longitude: number }, b: { latitude: number; longitude: number }) {
+//     return Math.sqrt(
+//         Math.pow(a.latitude - b.latitude, 2) + 
+//         Math.pow(a.longitude - b.longitude, 2)
+//     );
+// }
+
+function distance ( a: { latitude: number; longitude: number }, b: { latitude: number; longitude: number }) {
+    let dlat = (b.latitude - a.latitude) * Math.PI / 180;
+    let dlon = (b.longitude - a.longitude) * Math.PI / 180;
+    const lat1 = a.latitude * Math.PI / 180;
+    const lat2 = b.latitude * Math.PI / 180;
+
+    let x = Math.sin(dlat/2) * Math.sin(dlat/2) +
+            Math.sin(dlon/2) * Math.sin(dlon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    let c = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1-x)); 
+    let R = 6371;
+    return R * c;
 }
+
+
 
 
 export const findNearestRoutePoint= async (
@@ -27,6 +42,9 @@ export const findNearestRoutePoint= async (
             minDistance = calculatedDistance;
             bestPoint = point;
         }
+    }
+    if (minDistance > 1) {
+        throw new Error("No nearby route points found");
     }
     if(!bestPoint) {
         throw new Error("No route points available");
@@ -56,9 +74,9 @@ export const findBestDriver = async (pickupPoint: {
             { latitude: driver.currentLat!, longitude: driver.currentLng! },
             { latitude: pickupPoint.lat, longitude: pickupPoint.lng }
         );  
-    if (calculatedDistance > 0.02) continue; 
-
-    const score = calculatedDistance;
+    if (calculatedDistance > 1) continue; 
+    const passengerDistance = 0;
+    const score = calculatedDistance + passengerDistance;
 
     if (score < minDistance) {
         minDistance = score;
@@ -73,6 +91,10 @@ export const findBestDriver = async (pickupPoint: {
 
 
 export const requestRide = async (passengerId: string, pickupLat: number, pickupLng: number) =>  {
+    if (pickupLat == null || pickupLng == null) {
+        throw new Error("Invalid coordinates");
+    }
+
     const pickUpPoint = await findNearestRoutePoint(pickupLat, pickupLng);
     if(!pickUpPoint ) {
         throw new Error("No routes available nearby");
@@ -87,12 +109,15 @@ export const requestRide = async (passengerId: string, pickupLat: number, pickup
         throw new Error("You already have an active ride");
     }
     const bestDriver = await findBestDriver( pickUpPoint );
-    await prisma.driver.update({
-        where: { id: bestDriver.id },
-        data: { isAvailable: false }
-    });
-    const ride = await prisma.ride.create({
-        data: {
+    
+
+    const [ , ride] = await prisma.$transaction([
+        prisma.driver.update({
+            where: { id: bestDriver.id },
+            data: { isAvailable: false }
+        }),
+        prisma.ride.create({
+            data: {
             passengerId,
             pickupLat,
             pickupLng,
@@ -100,24 +125,39 @@ export const requestRide = async (passengerId: string, pickupLat: number, pickup
             driverId: bestDriver.id,
             status: "Requested"
         }
-    });
-
+        })
+    ]);
     return ride;
 }
 
 
 
 export const acceptRide = async (rideId: string, driverId: string) => {
+    const ride = await prisma.ride.findFirst({
+        where: { 
+            id: rideId,
+            status: "Requested",
+            driverId
+         }
+    });
+    if (!ride) {
+        throw new Error("Ride not found");
+    }
+    if(ride.status !== "Requested") {
+        throw new Error("Ride is not in a state to be accepted");
+    }
     return await prisma.ride.update({
         where: { id: rideId },
-        data: { status: "Accepted", driverId }
+        data: { status: "Accepted" }
     });
 }
 
+
 export const  completeRide = async (rideId: string) => {
-    const ride = await prisma.ride.findUnique({
+    const ride = await prisma.ride.update({
         where: { id: rideId },
-        select: { driverId: true }
+        select: { driverId: true },
+        data:{ status: "Completed" }
     });
     if(!ride) {
         throw new Error("Ride not found");
@@ -130,20 +170,33 @@ export const  completeRide = async (rideId: string) => {
     }
     return ride
 }   
+
+
 export const cancelRide = async (rideId: string) => {
-  const ride = await prisma.ride.update({
-    where: { id: rideId },
-    data: { status: "Cancelled" }
-  });
 
-  if (ride.driverId) {
-    await prisma.driver.update({
-      where: { id: ride.driverId },
-      data: { isAvailable: true }
+    const ride = await prisma.ride.findUnique({
+        where: { id: rideId },
+        data: { status: "Cancelled" }
     });
-  }
+    if(!ride) {
+        throw new Error("Ride not found");
+    }
 
-  return ride;
+    await prisma.$transaction([
+    prisma.ride.update(
+        {
+            where: { id: rideId },
+            data: { status: "Cancelled" }
+        }
+    ),
+    prisma.driver.update(
+        {
+            where: { id: ride.driverId! },
+            data: { isAvailable: true }
+        }   
+    )
+    ]);
+    return {message : "Ride cancelled successfully"};
 };    
  
 export const updateDriverLocation = async (driverId: string, lat: number, lng: number) => {
@@ -164,3 +217,105 @@ export const getRideStatus = async (rideId: string) => {
     }
     return ride.status;
 }
+
+
+export const rideHistory = async (passengerId: string) => {
+    return await prisma.ride.findMany({
+        where:{passengerId},
+        orderBy: { createdAt: "desc" }
+    })
+}
+
+export const allRidesByPassenger = async (passengerId: string) => {
+    const ride = await prisma.ride.findMany({
+        where: { passengerId },
+        include: {
+            driver: {
+                select: {
+                    user:{
+                        select:{
+                            name: true
+                        }
+                    },
+                    currentLat: true,
+                    currentLng: true
+                }
+            }
+        }
+    });
+    return ride;
+}
+
+export const getCurrentRides = async (passengerId: string) => {
+    const ride = await prisma.ride.findMany({
+        where: {
+            passengerId,
+            status:{
+                in: ["Requested", "Accepted"]
+            }
+        }, 
+        include: {
+            driver: {
+                select: {
+                    user : {
+                        select: {
+                            name: true
+                        }
+                    },
+                    currentLat: true,
+                    currentLng: true
+                }
+            }
+        }
+    });
+    return ride;
+}
+
+export const rideDetails = async (rideId: string) => {
+    const ride = await prisma.ride.findUnique({
+        where: { id: rideId },
+        include: {
+            driver: {
+                select: {
+                    user : {
+                        select: {
+                            name: true
+                        }
+                    },
+                    currentLat: true,
+                    currentLng: true
+                }
+            },
+            passenger: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    });
+    return ride;
+}
+
+export const allRidesByDriver = async (driverId: string) => {
+    const ride = await prisma.ride.findMany({
+        where: { driverId },
+        include: {
+            passenger: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    });
+    return ride;
+}
+
+export const driverAvailability = async (driverId: string, isAvailable: boolean) => {
+    return await prisma.driver.update({
+        where: { id: driverId },
+        data: { isAvailable }
+    });
+}
+
+
+
